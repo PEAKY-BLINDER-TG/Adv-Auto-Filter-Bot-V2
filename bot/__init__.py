e5c76d6bda9b6a440a283f910a392f07e131e8ca
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-14 20:28:47 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,15 +11,15 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = "3020564"
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = "91c026fadfdc442f504a0bd3e5c8cd18"
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = "1853498659:AAFdnSWzBEA4wqp8qpnViDKyvOSjV4y9-vU"
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = "mongodb+srv://Wafikh:wafikh@cluster0.pjcpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-USER_SESSION = os.environ.get("USER_SESSION")
+USER_SESSION = "BQCR0BxaDQFeM9wG9OjLeg4VfgQdVSaYfWw0ViuypH-delDV2DwbOe6w1vnR9XHNoH8DQ_643spGXP9fy-VR44KNybmXgYiJKP3wLNR-JuD5FEM5dSVptK8OzJtiv_oX5QPW3ibxQlqGLu0X2jlwLLX_m_oMabdyiN6yrtIhV-goKQyP7aGVktC4tossKhDTEAmiz3rHlvh9FUSd9_AbJphWZLvIbvg4Xt3yoBWectiXfXc1C1JjGeHUaEMqugDCun3gtggItmYx-3PqVgT2gjQ6IWsDINGbg1XD5FtgDEkKtAdqXxhOl7S3ZwAe4JvPCWy-0CZ9V5cw3Ow9LNLwaDjYXQWOpQA"
 
 VERIFY = {}
 
